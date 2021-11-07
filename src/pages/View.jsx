e2760642ff30698d7ab944f5716e19dcdfde37de
José Pereira Macedo <arseniumgx@gmail.com>
2021-11-07 13:57:39 -0300

    feat(View): contrução da tela de view com infos

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,18 +1,65 @@
 import { useState, useEffect } from "react"
-import { Link, Redirect } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import Api from "../services/api"
 
-function View(props) {
-	const [info, setInfo] = useState({})
+function View() {
+	const { id } = useParams()
 
-	useEffect(async () => {
-		const response = await Api.getChar(props.match.params.id)
-		const result = await response.json()
+	// Construção da estrutura esperada em list para não fica reclamado la em
+	// baixo que as propriedades não existem
+	const [info, setInfo] = useState({
+		id: 0,
+		name: "",
+		image: "",
+		episode: []
+	})
 
-		console.log(props.match.params.id)
+	const [load, setLoad] = useState(false)
 
-		setInfo(result)
+	const getNameEpisode = async (...id) => {
+		const response = await Api.getEpisode(id)
+		const { results } = await response.json()
+
+		const name = { results }
+
+		if (!name) return
+
+		setInfo({ ...info, episode: name })
+	}
+
+	useEffect(() => {
+		setInfo(async () => {
+			const response = await Api.getChar(id)
+			const result = await response.json()
+
+			info.id = result.id
+			info.name = result.name
+			info.image = result.image
+			info.episode = result.episode
+			console.log(result)
+
+			// info = {
+			// 	id: result.id,
+			// 	name: result.name,
+			// 	image: result.image,
+			// 	episode: result.episode
+			// }
+		})
+		setLoad(true)
 	}, [])
+
+	// useEffect(() => {
+	// 	info.episode.forEach((element, pos) => {
+	// 		if (!info.episode) return
+
+	// 		info.episode[pos] = +element.replace(
+	// 			"https://rickandmortyapi.com/api/episode/",
+	// 			""
+	// 		)
+	// 	})
+	// 	// console.log(info.episode)
+	// 	getNameEpisode(info.episode)
+	// }, [load])
 
 	return (
 		<section className='viewContent'>
@@ -25,12 +72,12 @@ function View(props) {
 			<span>
 				<p>{info.name}</p>
 			</span>
-			<h4>Appears</h4>
-			{/* <ul>
-				{info.episode.map((ep) => (
-					<li>{ep}</li>
-				))}
-			</ul> */}
+			<h4>Aparições</h4>
+			<ul>
+				{/* {info.episode.map(async (ep, pos) => {
+					return <li key={pos}>{ep}</li>
+				})} */}
+			</ul>
 		</section>
 	)
 }
